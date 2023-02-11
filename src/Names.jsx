fee439d2c323ce983ee8a2c7945628c1fbd36168
NachoCarvajal <nachoch64@correo.ugr.es>
2023-02-11 19:51:46 +0100

    Change project to work first on the optional part

--- src/Names.jsx
+++ src/Names.jsx
@@ -1,37 +1,35 @@
 import './App.css'
-import resType from './mocks/resultsTypes.json'
+import resPokemon from './mocks/results.json'
 
 function App() {
-  const pokemonList = resType.pokemon
-  // existe el tipo
-  const typeExists = resType.name != null
+  const pokemonExists = resPokemon && resPokemon.name
 
   return (
     <div className='App'>
       <header>
         <h1>Buscador Pokémon</h1>
         <form className='form'>
-          <input type='text' placeholder='Water, fire, dragon..' />
+          <input type='text' placeholder='Pikachu, Charmander...' />
           <button type='submit'>Search</button>
         </form>
       </header>
 
       <main>
         {
-          typeExists ? (
+          pokemonExists ? (
             <div className='pokemon'>
-
-              <h3>{resType.name}</h3>
+              <h3>{resPokemon.name}</h3>
               <ul>
                 {
-                  pokemonList.map((pokemon, index) => (
-                    <li key={index}>{pokemon.pokemon.name}</li>
+                  resPokemon.types.map((type, index) => (
+                    <li key={index}>{type.type.name}</li>
                   ))
                 }
               </ul>
+              <img src={resPokemon.sprites.front_default} alt={resPokemon.name} />
             </div>
           ) : (
-            <p>The type does not exist</p>
+            <p>No hay pokémon</p>
           )
         }
       </main>
